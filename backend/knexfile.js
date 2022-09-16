30d5176bb8992f92a361be111df79c7341fbe1ba
Tiago da Silva e Souza Pinto <tiago.souza9935@gmail.com>
2022-09-16 16:21:37 -0300

    config host

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   client: 'postgresql',
   connection: {
-    host: 'db',
+    host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     ssl: { rejectUnauthorized : false },
