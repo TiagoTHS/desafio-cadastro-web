7001e8e1df66e9633d31203dccfa546b161b8381
Tiago da Silva e Souza Pinto <tiago.souza9935@gmail.com>
2022-09-22 20:47:48 -0300

    Adicionando testes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   client: 'postgresql',
   connection: {
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized : false },
   },
   migrations: {
     tableName: 'knex_migrations'
